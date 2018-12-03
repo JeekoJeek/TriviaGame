f8abef04d91bd8e51d6d8c713c53840c0f5c0077
John Hansberry <johnvhansberry@gmail.com>
2018-12-03 14:30:09 -0500

    Q and A added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,7 +34,18 @@ $(document).ready(function(){
     //create on click function for displaying question card on begin game
     $(".begin-button").on("click", function(){
         $(".card").animate({opacity:1});
-        $(".card-header").html("<h2>" + questions(question) +"</h2>")
-    })
+        $(".card-header").text(questions[0].question);
+        $(".card-body").append("<button class='answers-1'>" + questions[0].answerChoices.a + "</button>");
+        $(".card-body").append("<button>" + questions[0].answerChoices.b + "</button>");
+        $(".card-body").append("<button>" + questions[0].answerChoices.c + "</button>");
+        $(".card-body").append("<button>" + questions[0].answerChoices.d + "</button>");
+
+        $(".answers-1").on("click", function(){
+            $(".card-body").append("<br><br><button> Submit </button>");
+        });
+
+    });
+
+
 
 })
